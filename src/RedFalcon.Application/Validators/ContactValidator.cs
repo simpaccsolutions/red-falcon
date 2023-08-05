c814215fc984ee8aa635ac0032f28927f5fb7d48
Diony S. Castro <dnycstr@gmail.com>
2023-08-05 08:37:35 +0800

    Add Sample Test

--- src/RedFalcon.Application/Validators/ContactValidator.cs
+++ src/RedFalcon.Application/Validators/ContactValidator.cs
@@ -7,8 +7,36 @@ namespace RedFalcon.Application.Validators
     {
         public async Task<bool> ValidateData(Contact value)
         {
+            if(value == null)
+                return false;
 
+            if(!ValidateFirstname(value.Firstname))
+                return false;
 
+            if (!ValidateEmail(value.Email))
+                return false;
+
+            return true;
+        }
+
+        private bool ValidateFirstname(string firstname)
+        {
+            if (string.IsNullOrEmpty(firstname))
+                return false;
+
+            if (firstname.Length > 100)
+                return false;
+
+            return true;
+        }
+
+        private bool ValidateEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+                return false;
+
+            if (email.Length > 100)
+                return false;
 
             return true;
         }
---
+++ test/RedFalcon.Application.Test/Validators/ContactValidatorTest.cs
@@ -0,0 +1,96 @@
+﻿using RedFalcon.Application.Interfaces.Validator;
+using RedFalcon.Application.Validators;
+using Xunit;
+
+namespace RedFalcon.Application.Test.Validators
+{
+    public class ContactValidatorTest
+    {
+        private readonly IContactValidator _validator;
+        public ContactValidatorTest()
+        {
+            _validator = new ContactValidator();
+        }
+
+        [Fact]
+        public void Validate_Test_Valid()
+        {
+            // Arrange
+            var contact = new Domain.Entities.Contact
+            {
+                Firstname = "John",
+                Lastname = "Doe",
+                Email = "john@app.co",
+                Phone = "1234567890",
+                BirthDate = DateTime.Parse("1990-01-01"),
+            };
+
+            // Act
+            var result = _validator.ValidateData(contact);
+
+            // Assert
+            Assert.True(result.Result);
+        }
+
+        [Fact]
+        public void Validate_Test_Invalid_Firstname_Empty()
+        {
+            // Arrange
+            var contact = new Domain.Entities.Contact
+            {
+                Firstname = "",
+                Lastname = "Doe",
+                Email = "john@app.co",
+                Phone = "1234567890",
+                BirthDate = DateTime.Parse("1990-01-01"),
+            };
+
+            // Act
+            var result = _validator.ValidateData(contact);
+
+            // Assert
+            Assert.False(result.Result);
+        }
+
+        [Fact]
+        public void Validate_Test_Invalid_Firstname_Length()
+        {
+            // Arrange
+            var contact = new Domain.Entities.Contact
+            {
+                Firstname = "asdfghjkloasdfghjkloasdfghjkloasdfghjkloasdfghjkloasdfghjkloasdfghjkloasdfghjkloasdfghjkloasdfghjkloasdfghjklo",
+                Lastname = "Doe",
+                Email = "john@app.co",
+                Phone = "1234567890",
+                BirthDate = DateTime.Parse("1990-01-01"),
+            };
+
+            // Act
+            var result = _validator.ValidateData(contact);
+
+            // Assert
+            Assert.False(result.Result);
+        }
+
+        [Fact]
+        public void Validate_Test_Invalid_Email_Length()
+        {
+            // Arrange
+            var contact = new Domain.Entities.Contact
+            {
+                Firstname = "John",
+                Lastname = "Doe",
+                Email = "asdfghjkloasdfghjkloasdfghjkloasdfghjkloasdfghjkloasdfghjkloasdfghjkloasdfghjkloasdfghjkloasdfghjkloasdfghjklo@app.co",
+                Phone = "1234567890",
+                BirthDate = DateTime.Parse("1990-01-01"),
+            };
+
+            // Act
+            var result = _validator.ValidateData(contact);
+
+            // Assert
+            Assert.False(result.Result);
+        }
+
+    }
+}
